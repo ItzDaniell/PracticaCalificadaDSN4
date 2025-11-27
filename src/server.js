baf52d4990fdb243d2c7b463b4ab13c30e13c1b4
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-11-26 19:09:48 -0500

    fix: remove https in production

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: false,
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24
   }
